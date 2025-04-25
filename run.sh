6eb98a9e2a5cb0450591a924a310402296fd48a1
virking <vir_king@outlook.com>
2025-04-25 14:55:32 +0800

    修改

--- run.sh
+++ run.sh
@@ -7,7 +7,7 @@ if [ "$current_hour" -ge 0 ] && [ "$current_hour" -lt 6 ]; then
     ./vnt-cli ${COMMAND} > /dev/null &
     # 计算到下一个 0 点的时间
     current_time=$(date +%s)
-    midnight=$(date -d "06:00" +%s)
+    midnight=$(date -d "05:59" +%s)
     wait_time=$((midnight - current_time - 10))
 
     echo "当前时间是 $(date +'%H:%M:%S')，将在 6 点退出，等待 $wait_time 秒..."
@@ -19,7 +19,7 @@ if [ "$current_hour" -ge 6 ] && [ "$current_hour" -lt 12 ]; then
     ./vnt-cli ${COMMAND} > /dev/null &
     # 计算到下一个 0 点的时间
     current_time=$(date +%s)
-    midnight=$(date -d "12:00" +%s)
+    midnight=$(date -d "11:59" +%s)
     wait_time=$((midnight - current_time - 10))
 
     echo "当前时间是 $(date +'%H:%M:%S')，将在 6 点退出，等待 $wait_time 秒..."
@@ -31,7 +31,7 @@ if [ "$current_hour" -ge 12 ] && [ "$current_hour" -lt 18 ]; then
     ./vnt-cli ${COMMAND} > /dev/null &
     # 计算到下一个 0 点的时间
     current_time=$(date +%s)
-    midnight=$(date -d "18:00" +%s)
+    midnight=$(date -d "17:59" +%s)
     wait_time=$((midnight - current_time - 10))
 
     echo "当前时间是 $(date +'%H:%M:%S')，将在 6 点退出，等待 $wait_time 秒..."
@@ -43,7 +43,7 @@ if [ "$current_hour" -ge 18 ] && [ "$current_hour" -lt 24 ]; then
     ./vnt-cli ${COMMAND} > /dev/null &
     # 计算到下一个 0 点的时间
     current_time=$(date +%s)
-    midnight=$(date -d "tomorrow 00:00" +%s)
+    midnight=$(date -d "23:59" +%s)
     wait_time=$((midnight - current_time - 10))
 
     echo "当前时间是 $(date +'%H:%M:%S')，将在 0 点退出，等待 $wait_time 秒..."
